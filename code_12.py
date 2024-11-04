aab483ef0db3496cadf76f3fd3b6cae42600b46d
sergivirgili <sergi.virgili.alonso@gmail.com>
2024-11-03 19:57:40 -0700

    Update code_12.py

--- code_12.py
+++ code_12.py
@@ -3,10 +3,15 @@ def get_fibonacci_number(position):
         return 0
     if position == 1:
         return 1
-    
+    return get_fibonacci_number(position - 2) + get_fibonacci_number(position-1)
+
 
 def get_fibonacci_number_sequence(number):
-    pass #Remove this line and insert your code here. Do not forget to use get_fibonacci_number to create your list of numbers.
+    if number == 0:
+        return [0]
+    if number == 1:
+        return [0,1]
+    
 
 if __name__ == "__main__":
-    pass #Remove this line and insert your code to test your Fibonacci function here
+    print(get_fibonacci_number(7))
